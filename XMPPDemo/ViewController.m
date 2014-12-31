b4f0ad5de585df39377148985de832d2dcef0cc1
Teddy Lin <teddy@Teddys-Mac-mini.local>
2014-12-31 15:41:56 +0800

    Initial Commit

---
+++ XMPPDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMPPDemo
+//
+//  Created by Teddy Lin on 12/31/14.
+//  Copyright (c) 2014 Zhimei Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ XMPPDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMPPDemo
+//
+//  Created by Teddy Lin on 12/31/14.
+//  Copyright (c) 2014 Zhimei Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
